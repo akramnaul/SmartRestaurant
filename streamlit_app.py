1145557c55f458d2b518b1a8aedb8532de8f2fca
akramnaul <60735293+akramnaul@users.noreply.github.com>
2024-12-29 16:03:38 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # from ui_components import render_header, render_data, render_sample_elements
 # from authentication import authenticate_user
 
-
+# Page Configuration
 st.set_page_config(
     page_title="Restaurant Management App",
     page_icon="🍽️",
@@ -12,5 +12,24 @@ st.set_page_config(
     initial_sidebar_state="auto",  # Options: "expanded", "collapsed", "auto"
 )
 
+# Custom CSS
+st.markdown(
+    """
+    <style>
+    body {
+        font-family: 'Arial', sans-serif;
+        font-size: 16px;
+    }
+    .stTitle {
+        font-size: 30px;
+        color: #2E86C1;
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("Welcome to the Restaurant Management App!")
 st.title("Sign In Page")
+

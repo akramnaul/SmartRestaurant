63fa26139413de1da9e1814ccafdc618e6ea2886
akramnaul <60735293+akramnaul@users.noreply.github.com>
2024-12-30 06:05:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import streamlit as st
 # from db_connection import get_mysql_data
 # from authentication import render_authentication_ui
 from ui_components import render_header, render_data, render_sample_elements
-from authentication import authenticate_user
 
 # Titles
 # st.title("SmartRestaurant : Sign In Page")

68a3ab8eda9b7bf3cfb29cf95a53cf3224ae13eb
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-01 02:50:06 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from mysql.connector import Error
 # from config import *
 from dotenv import load_dotenv
 import os
-from connect_db import *
+from database import *
 
 # Function to Execute a Stored Procedure
 def execute_stored_procedure(stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd"):

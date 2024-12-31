ad93ca0769dba6a5aa5380af28f643384dc41d41
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-01 02:43:48 +0500

    Create connect_db.py

---
+++ connect_db.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import mysql.connector
+from mysql.connector import Error
+# from config import *
+from dotenv import load_dotenv
+import os
+# Load environment variables from .env
+load_dotenv()
+
+# Function to Connect remote MySQL Database Server
+def connect_database():
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        if connection.is_connected():
+            # st.success("Successfully Connected MySQL Database : Rest ! ")
+            return connection
+        else:
+            st.error("Failed to Connect MySQL Database : Rest ! ")
+            return None
+    except Error as e:
+        st.error(f"Error: {e}")
+        return None

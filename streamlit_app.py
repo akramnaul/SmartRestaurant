950e2c896ad4abab63e6d3628bccb19924077490
akramnaul <60735293+akramnaul@users.noreply.github.com>
2024-12-31 09:25:53 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,31 +34,13 @@ def execute_stored_procedure(stored_procedure_name, inout_parameters):
         if connection is not None:
             cursor = connection.cursor()
 
-            # Define @variables for OUT Parameters
-            # out_vars = [f"@out{i}" for i in range(1, out_param_count + 1)]
-
-            # Build the parameter list for the procedure
-            # params = ", ".join(
-            #     [f"'{param}'" for param in in_params] + out_vars
-            # )
-
             # Execute the stored procedure
-            call_stored_procedure_query = f"CALL {stored_procedure_name}({inout_parameters});"
+            call_stored_procedure_query = f"CALL {stored_procedure_name}{inout_parameters};"
             cursor.execute(call_stored_procedure_query)
 
             st.write(f"pRestaurantUserName : {inout_parameters[3]}")
             st.write(f"pStatus : {inout_parameters[4]}")
             st.write(f"pStatusCheck : {inout_parameters[5]}")
-
-            # Retrieve the OUT parameter values
-            # out_values_query = f"SELECT {', '.join(out_vars)};"
-            # cursor.execute(out_values_query)
-            # out_values = cursor.fetchone()
-
-            # Display OUT parameters
-            # st.write("OUT Parameters:")
-            # for i, value in enumerate(out_values, start=1):
-            #     st.write(f"OUT Param {i}: {value}")
 
             cursor.close()
             connection.close()
@@ -70,21 +52,11 @@ def execute_stored_procedure(stored_procedure_name, inout_parameters):
 # Streamlit UI
 st.title("MySQL Database Connection and Stored Procedure Testing")
 
-# Input for stored procedure name
-# proc_name = st.text_input("Enter Stored Procedure Name", "your_procedure_name_here")
-# Input for IN parameters
-# in_params_input = st.text_area("Enter IN Parameters (comma-separated)", "Parameter1, Parameter 2, Parameter 3, ... ")
-# in_params = tuple(map(str.strip, in_params_input.split(','))) if in_params_input else ()
-
-# Input for OUT parameters
-# out_params_input = st.text_area("Enter OUT Parameters (comma-separated)", "Parameter1, Parameter 2, Parameter 3, ... ")
-# out_params = tuple(map(str.strip, out_params_input.split(','))) if out_params_input else ()
-
 # Execute the Stored Procedure when the Button is Pressed / Clicked
 if st.button("Test Stored Procedure"):
     stored_procedure_name = "RestaurantSignin"
     
-    parameters = "'pRestaurant','pRestaurantUser','pRestaurantUserPassword','pRestaurantUserName','pStatus','pStatusCheck'"
+    parameters = "('pRestaurant','pRestaurantUser','pRestaurantUserPassword','pRestaurantUserName','pStatus','pStatusCheck')"
 
     execute_stored_procedure(stored_procedure_name, parameters)
 

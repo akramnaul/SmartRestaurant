7a1bc337e28d5379b760ec1efbec96f5f5b9bb23
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-01 02:45:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,29 @@ from mysql.connector import Error
 # from config import *
 from dotenv import load_dotenv
 import os
-# Load environment variables from .env
-load_dotenv()
+from connect_db import connect_database
 
-# Function to Connect remote MySQL Database Server
-def connect_database():
-    try:
-        connection = mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME")
-        )
-        if connection.is_connected():
-            # st.success("Successfully Connected MySQL Database : Rest ! ")
-            return connection
-        else:
-            st.error("Failed to Connect MySQL Database : Rest ! ")
-            return None
-    except Error as e:
-        st.error(f"Error: {e}")
-        return None
+# Load environment variables from .env
+# load_dotenv()
+
+# # Function to Connect remote MySQL Database Server
+# def connect_database():
+#     try:
+#         connection = mysql.connector.connect(
+#             host=os.getenv("DB_HOST"),
+#             user=os.getenv("DB_USER"),
+#             password=os.getenv("DB_PASSWORD"),
+#             database=os.getenv("DB_NAME")
+#         )
+#         if connection.is_connected():
+#             # st.success("Successfully Connected MySQL Database : Rest ! ")
+#             return connection
+#         else:
+#             st.error("Failed to Connect MySQL Database : Rest ! ")
+#             return None
+#     except Error as e:
+#         st.error(f"Error: {e}")
+#         return None
 
 # Function to Execute a Stored Procedure
 def execute_stored_procedure(stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd"):

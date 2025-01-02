337f673588e0c429782f0a78610865e8493ff3dd
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-02 08:46:46 +0500

    Update and rename logic.py to logic_frontend.py

--- logic_frontend.py
+++ logic_frontend.py
@@ -3,7 +3,7 @@ import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
 import os
-from database import *
+from logic_backend import *
 
 # Function to Execute a Stored Procedure
 def execute_stored_procedure(stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd"):

7cb091b79c6f6f10a99d87795305934565ae7151
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-02 09:40:11 +0500

    Update logic_ui.py

--- frontend/logic_ui.py
+++ frontend/logic_ui.py
@@ -3,8 +3,8 @@ import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
 import os
-from database.connect_database import *
-from database.stored_procedures import *
+from .database.connect_database import *
+from .database.stored_procedures import *
 
 # Stored Procedure User Interface UI
 def stored_procedure_ui(): # stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd"):

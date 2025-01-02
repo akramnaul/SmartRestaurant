c1931c90381b7343b598136dbb519a2b707b8c5a
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-02 08:39:17 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import mysql.connector
 from mysql.connector import Error
-# from config import *
 from dotenv import load_dotenv
 import os
-# Load environment variables from .env
-# load_dotenv()
 
 # Function to Connect remote MySQL Database Server
 def connect_database():

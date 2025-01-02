d6e31ba800c8175adbbfa9a1b9073f87d7e09cab
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-02 08:35:12 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,37 +6,39 @@ from dotenv import load_dotenv
 import os
 from database import *
 from logic import *
+from interface import *
 
-# Stored Procedure User Interface UI
-def stored_procedure_ui(): # stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd"):
-    st.title("MySQL Database")
-    try:
-        connection = connect_database()
-        if connection is not None:
-            st.success("Successfully Connected DB : Rest ! ")
 
-            # Button to Trigger the Stored Procedure
-            if st.button("Click 2 Test : RestaurantSignin"):
-                stored_procedure_name = "RestaurantSignin"
+# # Stored Procedure User Interface UI
+# def stored_procedure_ui(): # stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd"):
+#     st.title("MySQL Database")
+#     try:
+#         connection = connect_database()
+#         if connection is not None:
+#             st.success("Successfully Connected DB : Rest ! ")
 
-                # Declare & Initialize the IN Parameters
-                pRestaurant = "KhanBurger"
-                pRestaurantUser = "03004444001"
-                pRestaurantUserPassword = "abcd"
+#             # Button to Trigger the Stored Procedure
+#             if st.button("Click 2 Test : RestaurantSignin"):
+#                 stored_procedure_name = "RestaurantSignin"
+
+#                 # Declare & Initialize the IN Parameters
+#                 pRestaurant = "KhanBurger"
+#                 pRestaurantUser = "03004444001"
+#                 pRestaurantUserPassword = "abcd"
             
-                # Call the Database Stored Procedure
-                result = execute_stored_procedure(stored_procedure_name, pRestaurant, pRestaurantUser, pRestaurantUserPassword)
-                # Display The Results
-                if result:
-                    st.write("Stored Procedure OUT Parameters :")
-                    # st.write(f"Result : {result}")
-                    st.write(f"pRestaurantUserName : {result['pRestaurantUserName']}")
-                    st.write(f"pStatus : {result['pStatus']}")
-                    st.write(f"pStatusCheck : {result['pStatusCheck']}")
-                else:
-                    st.error("Failed to execute stored procedure or retrieve results.")
-    except Error as e:
-        st.error(f"Error: {e}")
-        return None
+#                 # Call the Database Stored Procedure
+#                 result = execute_stored_procedure(stored_procedure_name, pRestaurant, pRestaurantUser, pRestaurantUserPassword)
+#                 # Display The Results
+#                 if result:
+#                     st.write("Stored Procedure OUT Parameters :")
+#                     # st.write(f"Result : {result}")
+#                     st.write(f"pRestaurantUserName : {result['pRestaurantUserName']}")
+#                     st.write(f"pStatus : {result['pStatus']}")
+#                     st.write(f"pStatusCheck : {result['pStatusCheck']}")
+#                 else:
+#                     st.error("Failed to execute stored procedure or retrieve results.")
+#     except Error as e:
+#         st.error(f"Error: {e}")
+#         return None
 
 stored_procedure_ui() # stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd")

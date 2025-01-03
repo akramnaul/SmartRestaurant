bc6f8763085af688de9eddb46a0c62845bf0eb45
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-03 15:05:19 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,21 +8,21 @@ from database.connect_database import connect_database
 from database.stored_procedures import execute_stored_procedure
 from frontend.logic_ui import stored_procedure_ui
 
-stored_procedure_call = (
-    f"CALL RestaurantSignin("
-    f"'KhanBurger', '03004444001', 'abcd',"            # IN Parameters
-    f"@pRestaurantUserName, @pStatus, @pStatusCheck);" # OUT Parameters
-)
+# stored_procedure_call = (
+#     f"CALL RestaurantSignin("
+#     f"'KhanBurger', '03004444001', 'abcd',"            # IN Parameters
+#     f"@pRestaurantUserName, @pStatus, @pStatusCheck);" # OUT Parameters
+# )
 
-result = execute_stored_procedure(stored_procedure_call)
+# result = execute_stored_procedure(stored_procedure_call)
 
-# Display the results
-if result:
-    st.write("Stored Procedure Output Parameters:")
-    st.write(f"pRestaurantUserName: {result['pRestaurantUserName']}")
-    st.write(f"pStatus: {result['pStatus']}")
-    st.write(f"pStatusCheck: {result['pStatusCheck']}")
-else:
-    st.error("Failed to execute the stored procedure or retrieve results.")
+# # Display the results
+# if result:
+#     st.write("Stored Procedure Output Parameters:")
+#     st.write(f"pRestaurantUserName: {result['pRestaurantUserName']}")
+#     st.write(f"pStatus: {result['pStatus']}")
+#     st.write(f"pStatusCheck: {result['pStatusCheck']}")
+# else:
+#     st.error("Failed to execute the stored procedure or retrieve results.")
 
-# stored_procedure_ui() # stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd")
+stored_procedure_ui() # stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd")

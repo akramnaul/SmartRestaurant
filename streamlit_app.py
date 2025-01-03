ecdbc2d132e6586d17991a91df6fe95492089bc9
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-03 15:03:02 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,15 @@ stored_procedure_call = (
     f"@pRestaurantUserName, @pStatus, @pStatusCheck);" # OUT Parameters
 )
 
-execute_stored_procedure(stored_procedure_call)
+result = execute_stored_procedure(stored_procedure_call)
+
+    # Display the results
+    if result:
+        st.write("Stored Procedure Output Parameters:")
+        st.write(f"pRestaurantUserName: {result['pRestaurantUserName']}")
+        st.write(f"pStatus: {result['pStatus']}")
+        st.write(f"pStatusCheck: {result['pStatusCheck']}")
+    else:
+        st.error("Failed to execute the stored procedure or retrieve results.")
 
 # stored_procedure_ui() # stored_procedure_name="RestaurantSignin",pRestaurant="KhanBurger",pRestaurantUser="03004444001",pRestaurantUserPassword="abcd")

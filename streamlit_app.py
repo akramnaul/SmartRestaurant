a4fec75167dc5b7fe68ddae9ec4887d004dfa283
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-15 17:04:37 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from mysql.connector import Error
 from dotenv import load_dotenv
 import os 
 from database.connect_database import connect_database
-from database.stored_procedures import execute_stored_procedure
+from database.execute_stored_procedure import execute_stored_procedure
 from frontend.restaurant_landing_page import signin
 
 signin()

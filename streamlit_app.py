ff04299950652ba6b66e3282c0afdec4334d94ae
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-01-19 08:45:18 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 import os 
 from database.connect_database import connect_database
 from database.execute_stored_procedure import execute_stored_procedure
-from frontend.restaurant_landing_page import user_signin
+# from frontend.restaurant_landing_page import user_signin
+from database.user_signin import verify_valid_user_signin
 
-user_signin()
+verify_valid_user_signin()
 

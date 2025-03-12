507bf73bd6ae0b3005e33962423eae5fd5d1d653
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-03-12 18:02:38 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import os
 from database.connect_database import connect_database
 from database.execute_stored_procedure import execute_stored_procedure
 # from frontend.restaurant_landing_page import user_signin
-from database.user_signin import verify_valid_user_signin, user_signin, setup_session_variables
+from database.user_signin import user_signin # , setup_session_variables, verify_valid_user_signin
 
 # if (verify_valid_user_signin() is False):
 #    st.write(".....Signin InValid.....")

f455637d177bef1ad9e4025551d61bd9868917a9
akramnaul <60735293+akramnaul@users.noreply.github.com>
2025-03-31 13:55:44 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,26 @@ import streamlit as st
 import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
-import os 
+import os
 from database.connect_database import connect_database
 from database.execute_stored_procedure import execute_stored_procedure
 # from frontend.restaurant_landing_page import user_signin
 from database.user_signin import user_signin
 
-if(user_signin() is True):
-  st.write("User Signin Successfully Reached : streamlit_app.py")
-else:
-  st.write("User Signin UnSuccessfully Reached : streamlit_app.py")
 
-# Check User's Roll / Class ... then ... Redirect to Appropriate Webpage
-if (('RestaurantUser' in st.session_state) and ('RestaurantUser' in st.session_state) and
-    ('RestaurantUserSigninValid' in st.session_state) and (st.session_state['RestaurantUserSigninValid'] is True)):
-    st.subheader("Smart Restaurant : Previous Valid Session & Signin")
-else:
-    st.subheader("Smart Restaurant : New Session & Signin")
+def get_ready_page():
+  if(user_signin() is True):
+    st.write("User Signin Successfully Reached Back @ streamlit_app.py")
+    # Check User's Roll / Class ... then ... Redirect to Appropriate Webpage
+    if (('Restaurant' in st.session_state) and
+        ('RestaurantUser' in st.session_state) and
+        ('RestaurantUserPassword' in st.session_state) and
+        ('RestaurantUserSigninValid' in st.session_state) and
+        (st.session_state['RestaurantUserSigninValid'] is True)):
+        st.subheader("Smart Restaurant : Get Ready Page : Successful")
+    else:
+        st.subheader("Smart Restaurant : Get Ready Page : UnSuccessful")
+
 
 # JavaScript to Reload the Page
 def clear_page():
@@ -29,6 +32,8 @@ def clear_page():
             location.reload();
         </script>
     """, unsafe_allow_html=True)
+
+    st.write("User Signin UnSuccessfully Reached Back @ streamlit_app.py")
 
 st.write("This is some content on the screen.")
 
